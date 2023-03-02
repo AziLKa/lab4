1b51d2143d72c88cd7f69d640bdba3698a808e87
AziLKa <vdovichevaelizaveta0104@mail.ru>
2023-03-03 00:49:10 +0300

    labababababababababababababababbabaabbabbababaababbaab

---
+++ z1.py
@@ -0,0 +1,6 @@
+password1 = input("Придумайте пароль: ")
+password2 = input("Повторите пароль: ")
+if password1 == password2:
+    print("Пароль зарегестрирован")
+else:
+    print("Пароли не совпадают")
