a6918957ec650c97776f131b30192cc21caae1ba
Liza <125025054+AziLKa@users.noreply.github.com>
2023-03-17 00:34:06 +0300

    Delete z1.py

--- z1.py
+++
@@ -1,6 +0,0 @@
-password1 = input("Придумайте пароль: ")
-password2 = input("Повторите пароль: ")
-if password1 == password2:
-    print("Пароль зарегестрирован")
-else:
-    print("Пароли не совпадают")
